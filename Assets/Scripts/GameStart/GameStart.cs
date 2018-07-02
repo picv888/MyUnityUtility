afcb8becd903132efac26202c67addb48c4faa5d
picv888 <274009365@qq.com>
2018-07-02 08:06:23 +0800

    添加音乐管理器

---
+++ Assets/Scripts/GameStart/GameStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 游戏开始的脚本，在这里用代码控制加载单例的组件，不需要在unity的setting里设置脚本加载顺序了
+/// </summary>
+public class GameStart : MonoBehaviour {
+
+    private static GameStart instance;
+    public static GameStart Instance{
+        get {
+            return instance;
+        }
+    }
+
+    private void Awake() {
+        instance = this;
+        Init();
+    }
+
+    GameObject m_gameObject;
+    /// <summary>
+    /// 加载单例的组件
+    /// </summary>
+    private void Init(){
+        m_gameObject = gameObject;
+        m_gameObject.AddComponent<AudioSourceManager>();
+    }
+}
